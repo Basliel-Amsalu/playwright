8bdf2f9b1c2ab91b418dddc1425cd1fb86981c30
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-07-02 14:49:41 +0300

    implementing waiting strategies and using variables

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -18,13 +18,20 @@ test.describe("Weather.com tests", () => {
   test("Navigate to Weather.com and search for a city", async () => {
     await page.goto("https://www.bbc.com/weather");
 
-    await page.click("input#ls-c-search__input-label");
-    await page.fill("input#ls-c-search__input-label", "Addis Ababa");
-    await page.press("input#ls-c-search__input-label", "Enter");
+    const searchBar = await page.waitForSelector(
+      "input#ls-c-search__input-label",
+      { timeout: 10000 }
+    );
+
+    await searchBar.click();
+    await searchBar.fill("Addis Ababa");
+    await searchBar.press("Enter");
 
     const option = page.getByRole("link", { name: "Addis Ababa, Ethiopia" });
     await expect(option).toBeVisible();
     await option.click();
+
+    await page.waitForSelector("#wr-forecast", { timeout: 10000 });
 
     await expect(
       page
