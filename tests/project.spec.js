878fd18e87d65c0e16e0128ece09341de0b42a62
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-07-02 14:53:52 +0300

    wrapping with try and catch blocks for error handling

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -6,41 +6,55 @@ test.describe("Weather.com tests", () => {
   let page;
 
   test.beforeAll(async () => {
-    browser = await test.chromium.launch({ headless: false });
-    context = await browser.newContext();
-    page = await context.newPage();
+    try {
+      browser = await test.chromium.launch({ headless: false });
+      context = await browser.newContext();
+      page = await context.newPage();
+    } catch (error) {
+      console.error("Error during setup:", error);
+    }
   });
 
   test.afterAll(async () => {
-    await browser.close();
+    try {
+      if (browser) {
+        await browser.close();
+      }
+    } catch (error) {
+      console.error("Error during teardown:", error);
+    }
   });
 
-  test("Navigate to Weather.com and search for a city", async () => {
-    await page.goto("https://www.bbc.com/weather", {
-      waitUntil: "networkidle",
-      timeout: 60000,
-    });
+  test("Navigate to BBC Weather and search for a city", async () => {
+    try {
+      await page.goto("https://www.bbc.com/weather", {
+        waitUntil: "networkidle",
+        timeout: 60000,
+      });
 
-    const searchBar = await page.waitForSelector(
-      "input#ls-c-search__input-label",
-      { timeout: 10000 }
-    );
+      const searchBar = await page.waitForSelector(
+        "input#ls-c-search__input-label",
+        { timeout: 10000 }
+      );
 
-    await searchBar.click();
-    await searchBar.fill("Addis Ababa");
-    await searchBar.press("Enter");
+      await searchBar.click();
+      await searchBar.fill("Addis Ababa");
+      await searchBar.press("Enter");
 
-    const option = page.getByRole("link", { name: "Addis Ababa, Ethiopia" });
-    await expect(option).toBeVisible();
-    await option.click();
+      const option = page.getByRole("link", { name: "Addis Ababa, Ethiopia" });
+      await expect(option).toBeVisible();
+      await option.click();
 
-    await page.waitForSelector("#wr-forecast", { timeout: 10000 });
+      await page.waitForSelector("#wr-forecast", { timeout: 10000 });
 
-    await expect(
-      page
-        .locator("#wr-forecast div")
-        .filter({ hasText: "Addis Ababa - Weather" })
-        .first()
-    ).toBeVisible();
+      await expect(
+        page
+          .locator("#wr-forecast div")
+          .filter({ hasText: "Addis Ababa - Weather" })
+          .first()
+      ).toBeVisible();
+    } catch (error) {
+      console.error("Error in navigating and searching:", error);
+    }
   });
 });
